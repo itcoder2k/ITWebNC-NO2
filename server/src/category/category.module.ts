0fbafefd2e94dcfde85feb5241ed87036c5ddc63
thangg_205 <23010098@st.phenikaa-uni.edu.vn>
2026-09-23 11:31:13 +0700

    Create category.module.ts

---
+++ server/src/category/category.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryService } from './category.service.js';
+import { CategoryController } from './category.controller.js';
+import { Category } from './entities/category.entity.js';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Category])],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+  exports: [CategoryService],
+})
+export class CategoryModule {}
